be5c2e1c7fd24543d53a3cc5fab614bf324ee8aa
AdamRB208 <adamrborup@gmail.com>
2025-03-18 15:10:14 -0600

    got art

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,13 +1,43 @@
-<script setup >
-import Example from '@/components/Example.vue';
+<script setup>
+import { artService } from '@/services/ArtService.js';
+import { logger } from '@/utils/Logger.js';
+import { Pop } from '@/utils/Pop.js';
+import { onMounted, onUnmounted } from 'vue';
+
+onMounted(() => {
+  logger.log('home page is mounted')
+  getArt()
+})
+
+onUnmounted(() => {
+  logger.log('home page is unmounted')
+
+})
+
+async function getArt() {
+  try {
+    await artService.getArt()
+  } catch (error) {
+    Pop.error(error, 'could not get art')
+    logger.error('could not get art', error)
+  }
+}
+
+
+
 
 
 </script>
 
 <template>
-  <Example />
+  <section class="container">
+    <div class="row">
+      <div class="col-md-3">images go here</div>
+    </div>
+  </section>
 </template>
 
-<style scoped lang="scss">
 
-</style>
+
+
+<style scoped lang="scss"></style>
